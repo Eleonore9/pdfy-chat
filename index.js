4618a9112388f7ddffef93cbb4700900b2e30eaa
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-09-28 17:45:51 +0200

    Route to post json data

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var app = express();
+var parser = require("body-parser");
 
 app.set('port', (process.env.PORT || 5000));
 
+
+// Test route
 app.get('/', function(request, response) {
   response.send('Hello World!')
 });
+
+
+// Allow external app to post JSON
+// Parse application/json
+app.use(parser.json())
+
+app.post('/create-pdf', function(request, response) {
+  console.log(request.body);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
