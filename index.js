3cf40dcfb0bd9fbd3f235c3a1642cefc1d5ef6e3
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-09-29 15:19:52 +0200

    Testing writing pdf file w/ node-jspdf library

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var parser = require("body-parser");
+var jsPDF = require("node-jspdf");
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,6 +10,9 @@ app.set('port', (process.env.PORT || 5000));
 // Test route
 app.get('/', function(request, response) {
   response.send('Hello World!')
+  var doc = jsPDF();
+  doc.text(20, 20, 'Hello, world.');
+  doc.save('Test.pdf', function(err){console.log('saved!');});
 });
 
 
